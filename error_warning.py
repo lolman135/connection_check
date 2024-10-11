da25766bbb2d4a0ab7876f9cf62e15b8371bcf2f
lolman135 <kkirilll2006@gmail.com>
2024-10-11 18:32:52 +0300

    Fix reboot bug

--- error_warning.py
+++ error_warning.py
@@ -5,5 +5,7 @@ class Error:
         answear = ms.askquestion("ERROR", "соединение потеряно\nВы хотите закрыть программу?")
         if answear == "yes":
             return "y"
-        
-        
+        else:
+            shutdown_response = ms.askquestion("Shutdown", "Вы хотите перезагрузить систему?")
+            if shutdown_response == "yes":
+                return "r"
